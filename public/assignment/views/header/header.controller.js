46934a718845831bbdacb307e4ac5eb240a18e2f
lyverc <lyverc@ccs.neu.edu>
2016-02-24 16:54:55 -0500

    Make a header controller. Add admin link to header

--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -1,3 +1,14 @@
 /**
  * Created by christopherlyver on 2/24/16.
  */
+(function() {
+    angular
+        .module("FormBuilderApp")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($scope) {
+
+    }
+
+
+})();
