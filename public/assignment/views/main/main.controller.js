52d67a8aac36d39d0c2b911f234f6d0bf956e14c
lyverc <lyverc@ccs.neu.edu>
2016-02-25 14:55:17 -0500

    Save the state of our MainController calling the UserService, to reference

--- public/assignment/views/main/main.controller.js
+++ public/assignment/views/main/main.controller.js
@@ -3,8 +3,18 @@
         .module("FormBuilderApp")
         .controller("MainController", MainController);
 
-    function MainController($scope, $location) {
-        $scope.loc = $location
+    function MainController($scope, $location, UserService) {
+        $scope.loc = $location;
+        $scope.users = "dddd";
+
+        $scope.setUsers= setUsers;
+
+        function call(res) {
+            $scope.users = res;
+        }
+        function setUsers() {
+            UserService.findAllUsers(call);
+        }
     }
 
 
