74c540ebbf7b90fa47e20e18a1fe232873ebdfc4
lyverc <lyverc@ccs.neu.edu>
2016-02-29 23:20:49 -0500

    Remove test call to the UserService

--- public/assignment/views/main/main.controller.js
+++ public/assignment/views/main/main.controller.js
@@ -5,14 +5,6 @@
 
     function MainController($scope, $location) {
         $scope.loc = $location;
-        $scope.getUsers = getUsers;
-
-        function call(res) {
-            $scope.users = res;
-        }
-        function getUsers() {
-            UserService.findAllUsers(call);
-        }
     }
 
 
