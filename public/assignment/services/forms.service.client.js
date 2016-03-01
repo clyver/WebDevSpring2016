d3619777a7aaa2d976d1b1d4f2ffc20b4636a1bc
lyverc <lyverc@ccs.neu.edu>
2016-02-29 19:48:06 -0500

    Break ground on FormsService

---
+++ public/assignment/services/forms.service.client.js
@@ -0,0 +1,71 @@
+/**
+ * Created by christopherlyver on 2/25/16.
+ */
+(function() {
+    angular
+        .module("FormBuilderApp")
+        .factory("FormService", FormService);
+
+    function FormService() {
+        var forms = [ {"_id": "000", "title": "Contacts", "userId": 123},
+                      {"_id": "010", "title": "ToDo",     "userId": 123},
+                      {"_id": "020", "title": "CDs",      "userId": 234}];
+
+        var service = {
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
+        };
+
+        return service;
+
+        function createFormForUser(userId, form, callback) {
+            form._id = (new Date).getTime();
+            form.userId = userId;
+            forms.push(form);
+            callback(forms);
+        }
+
+        function findAllFormsForUser(userId, callback) {
+            var user_forms = [];
+            var forms_len = forms.length;
+            for (var i = 0; i < forms_len; i++) {
+                var form = forms[i];
+                if (form.userId === userId) {
+                    user_forms.push(form);
+                }
+            }
+            callback(user_forms);
+        }
+
+        function deleteFormById(formId, callback) {
+            // Delete the given form from our list of forms
+            var form_to_delete = findFormIndexById(formId);
+            // Splice out the form_to_delete
+            forms.splice(form_to_delete, 1);
+            callback(forms);
+        }
+
+        function updateFormById(formId, newForm, callback) {
+            // Update the specified form
+            var form_to_update = findFormIndexById(formId);
+            forms[form_to_update] = newForm;
+            callback(forms);
+        }
+
+        function findFormIndexById(id) {
+            // A helper to find the index of the form with the given id
+            var forms_len = forms.length;
+            var form_index = -1;
+            for (var i = 0; i < forms_len; i++) {
+                var form = forms[i];
+                if (form._id === id) {
+                    form_index = i;
+                    break;
+                }
+            }
+            return form_index;
+        }
+    }
+})();
