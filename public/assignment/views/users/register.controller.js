0134dc6783a248dd0abeed7c1937bca63647b1e0
lyverc <lyverc@ccs.neu.edu>
2016-03-02 18:42:42 -0500

    When a user registers, assign them an id and a student role

--- public/assignment/views/users/register.controller.js
+++ public/assignment/views/users/register.controller.js
@@ -12,6 +12,9 @@
         $scope.register = register;
 
         function register(new_user) {
+            new_user._id = (new Date()).getTime();
+            // I'll assume only a student registration
+            new_user.roles =  ["student"];
             UserService.createUser(new_user, function(res) {
                 $rootScope.currentUser = new_user;
                 $location.url("/profile");
