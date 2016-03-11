9f6ab15d1b42b948f7ba062357cc4714b6705760
lyverc <lyverc@ccs.neu.edu>
2016-03-11 13:37:41 -0500

    When on mobile, bring sidbar links to nav-bar

--- public/project/views/header/header.controller.js
+++ public/project/views/header/header.controller.js
@@ -9,8 +9,23 @@
 
     function HeaderController($scope, $rootScope) {
         $scope.logout = logout;
+        $scope.switchMode = switchMode;
+
         function logout() {
             $rootScope.currentUser = null;
+        }
+
+        function switchMode() {
+            if ($rootScope.mentorMode) {
+                $rootScope.mentorMode = false;
+                $rootScope.apprenticeMode = true;
+                $rootScope.mode = "Sought Skills:";
+            } else {
+                $rootScope.mentorMode = true;
+                $rootScope.apprenticeMode = false;
+                $rootScope.mode = "Taught Skills:";
+            }
+            $rootScope.$emit('switchMode', []);
         }
     }
 
