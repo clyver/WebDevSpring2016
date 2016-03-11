c922ea2fcd6746a49378f83cd5aac1edb72ae665
lyverc <lyverc@ccs.neu.edu>
2016-03-11 11:36:07 -0500

    Allow the user to switch back and forth between mentor/apprentice mode

--- public/project/views/main/main.controller.js
+++ public/project/views/main/main.controller.js
@@ -7,7 +7,24 @@
         .module("SkillSharer")
         .controller("MainController", MainController);
 
-    function MainController($scope, $location) {
+    function MainController($scope, $rootScope, $location) {
         $scope.$location = $location;
+        $rootScope.mentorMode = true;
+        $rootScope.apprenticeMode = false;
+        $scope.currentMode = currentMode;
+
+        function currentMode() {
+            if ($scope.mentorMode) {
+                $rootScope.mode = "Taught Skills";
+            } else {
+                $rootScope.mode =  "Sought Skills";
+            }
+        }
+
+        (function init() {
+            currentMode()
+        })();
+
+
     }
 })();
--- public/project/views/sidebar/sidebar.controller.js
+++ public/project/views/sidebar/sidebar.controller.js
@@ -2,3 +2,27 @@
  * Created by christopherlyver on 3/1/16.
  */
 "use strict";
+(function() {
+    angular
+        .module("SkillSharer")
+        .controller("SidebarController", SidebarController);
+
+    function SidebarController($scope, $rootScope) {
+        $scope.switchMode = switchMode;
+
+        function switchMode() {
+            if ($rootScope.mentorMode) {
+                $rootScope.mentorMode = false;
+                $rootScope.apprenticeMode = true;
+                $rootScope.mode = "Sought Skills:";
+            } else {
+                $rootScope.mentorMode = true;
+                $rootScope.apprenticeMode = false;
+                $rootScope.mode = "Taught Skills:";
+
+            }
+
+        }
+    }
+
+})();
