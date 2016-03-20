a8958fdc4e42bf25f4eb20217006d7c9f95bcd51
lyverc <lyverc@ccs.neu.edu>
2016-03-20 18:44:24 -0400

    Update the routing with the new dir structure.  Update the server

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -10,25 +10,25 @@
     function config($routeProvider) {
         $routeProvider
             .when('/home', {
-                templateUrl: 'views/home/home.view.html'
+                templateUrl: 'client/views/home/home.view.html'
             })
             .when('/profile', {
-                templateUrl: 'views/users/profile.view.html',
+                templateUrl: 'client/views/users/profile.view.html',
                 controller: 'ProfileController'
             })
             .when('/admin', {
-                templateUrl: 'views/admin/admin.view.html'
+                templateUrl: 'client/views/admin/admin.view.html'
             })
             .when('/forms', {
-                templateUrl: 'views/forms/forms.view.html',
+                templateUrl: 'client/views/forms/forms.view.html',
                 controller: 'FormController'
             })
             .when('/register', {
-                templateUrl: 'views/users/register.view.html',
+                templateUrl: 'client/views/users/register.view.html',
                 controller: 'RegisterController'
             })
             .when('/login', {
-                templateUrl: 'views/users/login.view.html',
+                templateUrl: 'client/views/users/login.view.html',
                 controller: 'LoginController'
             })
             .otherwise({
--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,10 +1,13 @@
 /**
  * Created by christopherlyver on 3/20/16.
  */
-module.exports = function (app) {
-    var userService = require("./services/user.service.server.js")(app,userModel,null);
-    var formService = require("./services/form.service.server.js")(app,formModel,null);
-    var fieldService = require("./services/field.service.server.js")(app,formModel,null);
-    var userModel = require('./models/user.model.js')();
-    var formModel = require('./models/form.model.js')();
-}
+(function() {
+    'use strict';
+    module.exports = function (app) {
+        var userModel = require(__dirname + '/models/user.model.js')();
+        require(__dirname + '/services/user.service.server.js')(app, userModel);
+        var formModel = require(__dirname + '/models/form.model.js')();
+        require(__dirname + '/services/form.service.server.js')(app, formModel);
+        require(__dirname + '/services/field.service.server.js')(app, formModel);
+    };
+})();
