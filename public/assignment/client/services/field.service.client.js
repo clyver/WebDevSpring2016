f13478713c48d747a0e2fc22c40eed2fd064e12e
lyverc <lyverc@ccs.neu.edu>
2016-03-20 18:26:49 -0400

    Initial work on the field service (client)

---
+++ public/assignment/client/services/field.service.client.js
@@ -0,0 +1,46 @@
+/**
+ * Created by christopherlyver on 3/20/16.
+ */
+'use strict';
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .factory("FieldService", FormService);
+
+    function FieldService($http) {
+        var api = {
+            findFieldsForForm: findFieldsForForm,
+            createFieldForForm: createFieldForForm,
+            getFieldsForForm: getFieldsForForm,
+            getFieldForForm: getFieldForForm,
+            deleteFieldFromForm: deleteFieldFromForm,
+            updateField: updateField
+        };
+
+        return api;
+
+        function findFieldsForForm(formId) {
+            return $http.get("/api/assignment/form/" + formId + "/field");
+        }
+
+        function createFieldForForm(formId, field) {
+            return $http.post("/api/assignment/form/" + formId + "/field", field)
+        }
+
+        function getFieldsForForm(formId) {
+            return $http.get("/api/assignment/form/" + formId + "/field");
+        }
+
+        function getFieldForForm(formId, fieldId) {
+            return $http.get("/api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+
+        function deleteFieldFromForm(formId, fieldId) {
+            return $http.delete("/api/assignment/form/" + formId + "/field/" + fieldId)
+        }
+
+        function updateField(formId, fieldId, field) {
+            return $http.put("/api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+    }
+})();
