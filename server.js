0141aa3df9cf595dccb527db8fe23f8bd224dcfe
lyverc <lyverc@ccs.neu.edu>
2016-04-12 23:59:14 -0400

    Connect our app to the DB accordingly

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 var express = require('express');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610');
+
+var mongo_connection = 'mongodb://localhost/cs5610';
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    mongo_connection = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+mongoose.connect(mongo_connection);
 
 var bodyParser = require('body-parser');
 var app = express();
