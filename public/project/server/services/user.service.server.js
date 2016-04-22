6ddf15153be50484d7b621e7bd91f5334533f975
lyverc <lyverc@ccs.neu.edu>
2016-04-22 14:20:27 -0400

    Prepare server side user service

--- public/project/server/services/user.service.server.js
+++ public/project/server/services/user.service.server.js
@@ -1,3 +1,79 @@
 /**
  * Created by christopherlyver on 4/22/16.
  */
+module.exports = function(app, model) {
+
+    app.get("/api/assignment/user/username/:username/password/:password", findUserByCredentials);
+    app.get("/api/assignment/user", findAllUsers);
+    app.post("/api/assignment/admin/user", createUser);
+    app.delete("/api/assignment/user/:id", deleteUser);
+    app.put("/api/assignment/user/:id", updateUser);
+
+
+    function createUser(req, res) {
+        var new_user = req.body;
+
+        if (!new_user.roles) {
+            new_user.roles = ['student'];
+        }
+        model.createUser(new_user)
+            .then(function (user) {
+                res.json(user);
+            }, function (err) {
+                res.json({'error': err});
+            });
+    }
+
+    function updateUser(req, res) {
+        var userId = req.params.id;
+        var user = req.body;
+        //var updated_user = model.updateUser(userId, user);
+        //res.json(updated_user);
+
+        model.updateUser(userId, user)
+            .then(
+                function(doc) {
+                    res.json(doc);
+                },
+                function(err) {
+                    console.log("Service err:");
+                    console.log(err);
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function findAllUsers(req, res) {
+        var users = model.findAllUsers()
+            .then(
+                function(doc) {
+                    res.json(doc);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function findUserByCredentials(req, res) {
+        var username = req.params.username;
+        var password = req.params.password;
+
+        model
+            .findUserByCredentials(username, password)
+            .then(
+                function(doc) {
+                    res.json(doc);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteUser(req, res) {
+        var userId = req.params.id;
+        var deletedUser = model.deleteUser(userId);
+        res.json(deletedUser);
+    }
+};
