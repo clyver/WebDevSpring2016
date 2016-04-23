61943e4b522ad62eb5b61f075702031566d188bc
lyverc <lyverc@ccs.neu.edu>
2016-04-22 23:17:36 -0400

    Remove notion of mentor/apprentice. Everyone is each

--- public/project/client/views/users/register.controller.js
+++ public/project/client/views/users/register.controller.js
@@ -13,25 +13,15 @@
 
         function register(new_user) {
             new_user._id = (new Date()).getTime();
-            new_user.roles = [];
-            if (new_user.mentor) {
-                new_user.roles.push("mentor");
-            }
-            if (new_user.apprentice){
-                new_user.roles.push("apprentice");
-            }
 
-            // Only register the user if they select at least one role
-            if (new_user.roles.length) {
-                UserService.createUser(new_user).then(
-                    function (response) {
-                        if (response.data) {
-                            $rootScope.currentUser = new_user;
-                            $location.url("/profile");
-                        }
+            UserService.createUser(new_user).then(
+                function (response) {
+                    if (response.data) {
+                        $rootScope.currentUser = response.data;
+                        $location.url("/profile");
                     }
-                );
-            }
+                }
+            );
         }
     }
 })();
